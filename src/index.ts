48bef5d10f4b43e98f5dc4ee72fb0704d6bbc2b4
liangshuo <liangshuoone@aliyun.com>
2024-02-01 17:30:11 +0800

    wip:～

--- src/index.ts
+++ src/index.ts
@@ -2,48 +2,82 @@
  * @Author: liangshuo liangshuoone@aliyun.com
  * @Date: 2024-01-31 18:19:57
  * @Description: Analyize
- * @LastEditTime: 2024-01-31 18:30:39
+ * @LastEditTime: 2024-02-01 17:29:01
  * @LastEditors: liangshuo liangshuoone@aliyun.com
  */
+declare interface Window {
+    Analayze: typeof Analayze
+}
 
 type Config = {
     base_url: string
+    rest_data?: Record<string, unknown>
 }
+
 class Analayze {
     /** Burial Point Address  */
     protected base_url: string
-
+    protected rest_data: Record<string, unknown>
 
     constructor(config: Config) {
         this.base_url = config.base_url
-
-        this.start_sa()
+        this.rest_data = config.rest_data || {}
+        this.startGather()
     }
 
     // Start Burial
-    public start_sa() {
+    public startGather() {
         this.gatherLoadData()
+        this.gatherClick()
     }
+
+    public generateBaseData() {
+        const pageUrl = window.location.href;
+        const timeStamp = new Date().getTime();
+
+        return {
+            pageUrl,
+            timeStamp
+        }
+    }
+
 
     // Burial Load
     public gatherLoadData() {
         window.addEventListener('load', () => {
-            const pageUrl = window.location.href;
-            const timeStamp = new Date().getTime()
+
             const userData = {
+                ...this.rest_data,
                 eventType: 'pageView',
-                pageUrl,
-                timeStamp
+                ...this.generateBaseData()
             }
             this.sendData(userData)
         })
     }
 
+    // Burial Click
+    public gatherClick() {
+        window.addEventListener('click', (e) => {
+            const userData = {
+                ...this.rest_data,
+                eventType: 'click',
+                ...this.generateBaseData(),
+                position: {
+                    pageX: e.pageX,
+                    pageY: e.pageY
+                }
+            }
+            console.log(e, 'e')
+            this.sendData(userData)
+        })
+    }
+
+
     public sendData(data: any) {
+        // const xhr = new XMLHttpRequest()
         console.log('sendData', data)
     }
 
 }
 
-
-export default Analayze;
+window.Analayze = Analayze
